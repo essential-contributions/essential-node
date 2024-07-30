d3ccb0a32eb6fa8066e8560484583ad5488a6e6c
mitchmindtree <mitchell.nordine@essentialcontributions.com>
2024-07-30 10:00:32 +1000

    fix: Use std id primary key for block_solution table

--- crates/node-db-sql/sql/create/block_solution.sql
+++ crates/node-db-sql/sql/create/block_solution.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS block_solution (
+    id INTEGER PRIMARY KEY,
     block_number INTEGER NOT NULL,
     solution_id INTEGER NOT NULL,
     solution_index INTEGER NOT NULL,
     FOREIGN KEY (block_number) REFERENCES block (number),
     FOREIGN KEY (solution_id) REFERENCES solution (id),
-    PRIMARY KEY (block_number, solution_id, solution_index)
+    UNIQUE(block_number, solution_id, solution_index)
 );
