d007252d47756a7b63856ff711ed4c623a4bdc0f
Tom <tomrgowan@gmail.com>
2024-10-18 17:46:28 +1100

    fix: infinite loop

--- crates/node/src/state_derivation.rs
+++ crates/node/src/state_derivation.rs
@@ -35,7 +35,7 @@ where
 /// Critical errors will cause the stream to end.
 pub fn state_derivation_stream(
     conn_pool: ConnectionPool,
-    block_rx: watch::Receiver<()>,
+    mut block_rx: watch::Receiver<()>,
 ) -> Result<Handle<CriticalError>, CriticalError> {
     let (shutdown, stream_close) = watch::channel(());
 
@@ -43,10 +43,12 @@ pub fn state_derivation_stream(
         let mut missing_block = false;
         loop {
             let (self_notify, self_notify_rx) = watch::channel(());
+            let b = block_rx.clone();
+            block_rx.mark_unchanged();
             let rx = if missing_block {
-                WatchStream::from_changes(block_rx.clone())
+                WatchStream::from_changes(b)
             } else {
-                WatchStream::new(block_rx.clone())
+                WatchStream::new(b)
             };
             let rx = futures::stream::select(rx, WatchStream::from_changes(self_notify_rx));
             let mut stream_close = stream_close.clone();
